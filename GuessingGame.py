3f993ed2069c5f5dc1c0b5e7a8e25f9257cc8371
badfire <badfire@AMD-AUSTIN>
2024-04-07 19:10:19 -0500

    text fix

--- GuessingGame.py
+++ GuessingGame.py
@@ -23,7 +23,7 @@ def play_game(limit):
 def main():
     display_the_heading()
     while True:
-        limit = int(input("Enter the limit for the range: "))
+        limit = int(input("Enter the max range: "))
         play_again = play_game(limit)
         if not play_again:
             print("Thanks for playing!")
